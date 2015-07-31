9a557dafc6e01a990c14106033cdd8ac46e4e8bf
Alex Miles Younger <alex@alexmilesyounger.com>
2015-07-31 13:58:03 -0400

    Initial commit

---
+++ badpep8.py
@@ -0,0 +1,20 @@
+# multiple imports
+def fooBar(arg1, arg2, arg3, arg4):
+		# way too much indentation
+		return arg1, arg2, arg3, arg4
+def bar(*args):
+    # bad spacing
+    return 2+2
+
+
+# Bad class name, bad spacing, bad indentation
+class treehouse:
+	def one(self):
+	  return 1
+	def two(self):
+	  return 2
+
+import sys, random
+
+
+# bad indentation and whitespace
