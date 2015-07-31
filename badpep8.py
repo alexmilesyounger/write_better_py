47824b3c8631577d36d75c672f369b5d28b49bc0
Alex Miles Younger <alex@alexmilesyounger.com>
2015-07-31 14:01:19 -0400

    Fix indentation and whitespacing

--- badpep8.py
+++ badpep8.py
@@ -1,18 +1,21 @@
 # multiple imports
 def fooBar(arg1, arg2, arg3, arg4):
-		# way too much indentation
-		return arg1, arg2, arg3, arg4
+	# way too much indentation
+	return arg1, arg2, arg3, arg4
+
+
 def bar(*args):
-    # bad spacing
-    return 2+2
+	# bad spacing
+	return 2+2
 
 
 # Bad class name, bad spacing, bad indentation
 class treehouse:
 	def one(self):
-	  return 1
+		return 1
+	
 	def two(self):
-	  return 2
+		return 2
 
 import sys, random
 
